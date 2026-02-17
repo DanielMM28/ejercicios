a5dbd81b8b4eecb005d056d6bf960d5232fbc2ba
DanielMM28 <danieldavidmu9@gmail.com>
2026-02-17 17:54:06 -0500

    semanas

--- ejercicio1/ejercicio.js
+++ ejercicio1/ejercicio.js
@@ -3,28 +3,44 @@ document.getElementById("formulario").addEventListener("submit", function(e) {
 
     const nombre = document.getElementById("nombre").value;
     const fechaNacimiento = new Date(document.getElementById("fechaNacimiento").value);
+    const fechaInicio = new Date(document.getElementById("fechaInicio").value); 
     const sexo = document.getElementById("sexo").value;
 
     const hoy = new Date();
-    let edadActual = hoy.getFullYear() - fechaNacimiento.getFullYear();
 
+   
+    let edadActual = hoy.getFullYear() - fechaNacimiento.getFullYear();
+   
     let edadPension = sexo === "mujer" ? 57 : 62;
     let añosFaltantes = edadPension - edadActual;
-  let diff = hoy - fechaInicio;
+
+    let diff = hoy - fechaInicio;
     let semanasCotizadas = Math.floor(diff / (1000 * 60 * 60 * 24 * 7));
 
-    // SEMANAS QUE FALTAN
     const SEMANAS_REQUERIDAS = 1300;
+    
     let semanasFaltantes = SEMANAS_REQUERIDAS - semanasCotizadas;
+
     let mensaje = "";
 
-    if (añosFaltantes > 0) {
-        mensaje = `a ${nombre}, de ${edadActual} años le faltan ${añosFaltantes} años y ${semanasFaltantes} semanas cotizadas para pensionarse.`;
-    } else {
+    if (edadActual >= edadPension && semanasCotizadas >= SEMANAS_REQUERIDAS) {
         mensaje = `${nombre}, ya puedes pensionarte 🎉`;
+    } else {
+        mensaje = `${nombre}, de ${edadActual} años, `;
+        
+        if (añosFaltantes > 0) {
+            mensaje += `te faltan ${añosFaltantes} años  y `;
+        } else {
+            mensaje += `ya cumples la edad, `;
+        }
+
+        if (semanasFaltantes > 0) {
+            mensaje += `te faltan ${semanasFaltantes} semanas cotizadas para pensionarte.`;
+        } else {
+            mensaje += `y ya cumples las semanas requeridas.`;
+        }
     }
 
     document.getElementById("resultado").innerText = mensaje;
 });
-
 
